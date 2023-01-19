406c58dfc81b8f03f24f15b98f4bd471cd6c9b8b
lorber190103 <lorber190103@sr.htlweiz.at>
2023-01-19 17:25:55 +0100

    Updated fill.py

--- fill.py
+++ fill.py
@@ -2,17 +2,22 @@ from faker import Faker
 from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import Session
-from model import Company
+from model import Company, Item
 
 engine = create_engine("sqlite:///inventory.db", echo=True)
 fake = Faker()
 Session = sessionmaker(bind = engine)
 session = Session()
 
-
 Faker.seed(0)
 
 for _ in range(5):
     session.add(Company(name=fake.company()))
+
+for _ in range(250):
+    session.add(Item(company_id=fake.random_int(min=1, max=5) ,
+                     name= None,
+                     quantity=fake.random_int(min=1, max=100),
+                     location=fake.bothify('?##',letters='ABCDE')))
 
 session.commit()
