12d9d266d01b304450f089bc77ecbc9f0dd7c24a
lorber190103 <lorber190103@sr.htlweiz.at>
2023-01-19 16:10:27 +0100

    Added ER-Diagramm.jpg and model.py

---
+++ model.py
@@ -0,0 +1,24 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import *
+
+
+metadata_obj = MetaData()
+Base = declarative_base()
+engine = create_engine("sqlite:///inventory.db", echo=True)
+
+class Company(Base):
+    __tablename__ = "company"
+
+    id = Column(Integer(), primary_key = True)
+    name = Column(String, nullable = False)
+
+class Item(Base):
+    __tablename__ = "item"
+
+    id = Column(Integer(), primary_key = True)
+    company_id = Column(Integer(), ForeignKey('company.id'))
+    name = Column(String, nullable = False)
+    quantity = Column(Integer, nullable = False)
+    location = Column(String, nullable = False)
+
+Base.metadata.create_all(engine)
