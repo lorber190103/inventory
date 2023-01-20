a473bfc10baa891444639a7e77d31936bc071f6a
lorber190103 <lorber190103@sr.htlweiz.at>
2023-01-20 10:38:28 +0100

    Updated fill.py and .gitignore

--- fill.py
+++ fill.py
@@ -8,16 +8,20 @@ engine = create_engine("sqlite:///inventory.db", echo=True)
 fake = Faker()
 Session = sessionmaker(bind = engine)
 session = Session()
-
+items = open("items.md", "r")
+item = items.read()
+item_into = item.split("\n")
 Faker.seed(0)
+i = 0
 
 for _ in range(5):
     session.add(Company(name=fake.company()))
 
 for _ in range(250):
     session.add(Item(company_id=fake.random_int(min=1, max=5) ,
-                     name= None,
+                     name=item_into[i],
                      quantity=fake.random_int(min=1, max=100),
                      location=fake.bothify('?##',letters='ABCDE')))
+    i = i+1
 
 session.commit()
