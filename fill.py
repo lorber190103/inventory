b63a42394cd81204eafebdd57776b990fe708471
lorber190103 <lorber190103@sr.htlweiz.at>
2023-01-25 10:38:18 +0100

    Django

--- fill.py
+++ fill.py
@@ -13,12 +13,11 @@ item_into = item.split("\n")
 Faker.seed(0)
 i = 0
 
-
 for _ in range(5):
     session.add(Company(name=fake.company()))
 
 for _ in range(250):
-    session.add(Item(company_id=fake.random_int(min=1, max=5) ,
+    session.add(Item(company_id=fake.random_int(min=1, max=5),
                      name=item_into[i],
                      quantity=fake.random_int(min=1, max=100),
                      location=fake.bothify('?##',letters='ABCDE')))
