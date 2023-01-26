13a93812c25bc37275ada20e07138fe5484daeb0
lorber190103 <lorber190103@sr.htlweiz.at>
2023-01-26 16:46:53 +0100

    Tried some things with Django

--- fill.py
+++ fill.py
@@ -11,16 +11,22 @@ items = open("items.md", "r")
 item = items.read()
 item_into = item.split("\n")
 Faker.seed(0)
-i = 0
 
-for _ in range(5):
-    session.add(Company(name=fake.company()))
 
-for _ in range(250):
-    session.add(Item(company_id=fake.random_int(min=1, max=5),
-                     name=item_into[i],
-                     quantity=fake.random_int(min=1, max=100),
-                     location=fake.bothify('?##',letters='ABCDE')))
-    i = i + 1
+def fill_company():
+    for _ in range(5):
+        session.add(Company(name=fake.company()))
+    session.commit()
 
-session.commit()
+def fill_item():
+    i = 0
+    for _ in range(250):
+        session.add(Item(company_id=fake.random_int(min=1, max=5),
+                         name=item_into[i],
+                         quantity=fake.random_int(min=1, max=100),
+                         location=fake.bothify('?##',letters='ABCDE')))
+        i = i + 1
+    session.commit()
+
+fill_company()
+fill_item()
